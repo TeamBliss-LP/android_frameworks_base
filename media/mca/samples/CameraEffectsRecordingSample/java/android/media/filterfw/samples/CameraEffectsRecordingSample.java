3b04a011a63bdf821773f8c4cbddec76c6cdfc63
PrimeDirective <activethrasher00@gmail.com>
2013-08-13 19:57:39 -0400

    potential GraphRunner leak in camera effects
    if the camera FCs while recording effects.. *poof* a tiny leak is born.
    so.. we close mRunner onDestroy.
    
    Change-Id: I6a7983f120a03137f545a3cd9bb07ff51a9cd2d7

--- media/mca/samples/CameraEffectsRecordingSample/java/android/media/filterfw/samples/CameraEffectsRecordingSample.java
+++ media/mca/samples/CameraEffectsRecordingSample/java/android/media/filterfw/samples/CameraEffectsRecordingSample.java
@@ -85,6 +85,13 @@ public class CameraEffectsRecordingSample extends Activity {
             mRunButton.setText("Record");
         }
     }
+    
+    protected void onDestroy() {
+		super.onDestroy();
+		if (mRunner.isRunning()) {
+			mRunner.close();
+		}
+	}
 
     private OnClickListener mRunButtonClick = new OnClickListener() {
         @Override
