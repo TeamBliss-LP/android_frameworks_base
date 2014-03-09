4e24d0ddcca22eeec8183d8b9267b0323872be3b
Jmz <jmztaylor@gmail.com>
2014-03-09 08:28:00 +0100

    Allow disabling of FC dialogs [1/2]
    Change-Id: If87aa74ebb45849f6e5fe3a53ce9beeaac28f624

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3949,6 +3949,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFCATION_DECAY = "heads_up_notification_decay";
 
         /**
+         * Disable FC Notifications
+         * @hide
+         */
+        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
