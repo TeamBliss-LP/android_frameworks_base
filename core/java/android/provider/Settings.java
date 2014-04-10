3a26ca960b2ff538b86a663ef529c55aba63c1ea
BigBrother1984 <stevewatersy@gmail.com>
2014-04-10 18:07:26 +0200

    Forward Port: Floating Window with Multitasking.
    Forward Port and Fixups done by BlackDragon && rogersb11
    
    Floating window contributors:
    @drcmda
    @beerbong
    @D4rKn3sSyS
    
    This feature, has ability to:
    1. Moveable window to various position
    2. Scaling window with 2 finger like zoom in/out
    3. Double tap in actionbar or above window to fullscreen or back to floating window
    4. Automatically resize when Snapping into one of side (Top, Bottom, Left, Right)
       (taken from xhalofloatingwindow by @zst123)
    5. Automatically switch focus from other app (taken from multiwindow by @xplodwild)
    6. Unlimited floating window app (depends on RAM)
    
    To make app launch using Floating window feature
    
    you must add Intent.FLAG_FLOATING_WINDOW
    before start an activity
    
    for example you can look at https://gerrit.omnirom.org/6515
    
    need https://gerrit.omnirom.org/#/c/6561
    
    PS.17 : fix scaling
    PS.18 : fix layout calculation when rotate
    PS.19 : support minimize/maximize window (see App circle sidebar)
    PS.20 : fix previouse app
    PS.21 : rebased due to changes in 4.4.3_r1.1
    PS.26 : rebased
    PS.27 : rebased
    PS.30 : a little touch :)
    PS.31 : fix FCs because package manager has died
    
    Fusion:
    
    PS.32 : Adapted to work on lollipop.
    PS.33 : Kill floating activities running in the same thread when tapping outside.
    PS.34 : Recents: Launch as Floating App.
    PS.35 : Added some tinted code by XplodWild.
    PS.36 : Remove whitespace.
    PS.37 : Added floating window intent to slimactions.
    PS.38 : Cleanup unused code.
    
    Rogersb11: Add appsidebar intent, slim recents intent and fix bug
    
    Todo: add more intents for some features to allow open in floating window.
    
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>
    Signed-off-by: rogersb11 <brettrogers11@gmail.com>
    Slim actions floating mode switch 2/2
    
    Change-Id: I94c2f29bb4c920b6cf894b59d43f79ef659a5988
    
    Signed-off-by: rogersb11 <brettrogers11@gmail.com>
    
    Floating window update.
    
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>
    
    Change-Id: I0dab65e32783e52b556811f9e70b046aac8f65c2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4980,6 +4980,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_HIDE_ARROW = "network_traffic_hide_arrow";
 
         /**
+         * Option to launch slim actions in floating mode
+         * @hide
+         */
+        public static final String SLIM_ACTION_FLOATS = "slim_action_floats";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4564,6 +4564,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
+    public Rect getContentRect() {
+        return new Rect(mContentLeft, mContentTop, mContentRight, mContentBottom);
+    }
+
     /** {@inheritDoc} */
     @Override
     public void layoutWindowLw(WindowState win, WindowState attached) {
