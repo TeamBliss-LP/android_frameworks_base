44b9d520e072363742296c8b0ebd784c4418b294
Alexander Martinz <eviscerationls@gmail.com>
2014-07-05 17:18:38 +0200

    (1/2) Frameworks: allow to toggle smart cover wake
      * enable by default
    
    Change-Id: I83d96696fefedc19fc3bce7265a4fcf2d4c894bc
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -435,6 +435,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int mLidKeyboardAccessibility;
     int mLidNavigationAccessibility;
     boolean mLidControlsSleep;
+    boolean mLidControlsWake;
     int mShortPressOnPowerBehavior;
     int mLongPressOnPowerBehavior;
     int mDoublePressOnPowerBehavior;
@@ -939,6 +940,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.ENABLE_HW_KEYS), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LID_CONTROLS_WAKE), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LID_CONTROLS_SLEEP), false, this,
                     UserHandle.USER_ALL);
 
             updateSettings();
@@ -1677,8 +1684,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 com.android.internal.R.integer.config_lidKeyboardAccessibility);
         mLidNavigationAccessibility = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_lidNavigationAccessibility);
-        mLidControlsSleep = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_lidControlsSleep);
         mTranslucentDecorEnabled = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_enableTranslucentDecor);
 
@@ -2244,6 +2249,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (mImmersiveModeConfirmation != null) {
                 mImmersiveModeConfirmation.loadSetting(mCurrentUserId);
             }
+
+            mLidControlsSleep = Settings.System.getIntForUser(resolver,
+                    Settings.System.LID_CONTROLS_SLEEP,
+                    1, UserHandle.USER_CURRENT) != 0;
+            mLidControlsWake = Settings.System.getIntForUser(resolver,
+                    Settings.System.LID_CONTROLS_WAKE,
+                    1, UserHandle.USER_CURRENT) != 0;
 
             WindowManagerPolicyControl.reloadFromSetting(mContext);
         }
@@ -5302,7 +5314,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         updateRotation(true);
 
         if (lidOpen) {
-            wakeUp(SystemClock.uptimeMillis(), mAllowTheaterModeWakeFromLidSwitch);
+            if (mLidControlsWake) {
+                wakeUp(SystemClock.uptimeMillis(), mAllowTheaterModeWakeFromLidSwitch);
+            }
         } else if (!mLidControlsSleep) {
             mPowerManager.userActivity(SystemClock.uptimeMillis(), false);
         }
