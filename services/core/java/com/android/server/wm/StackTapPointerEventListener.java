0663c77b7eeb53150ce4b3516fbc88556dad9e33
tingna_sung <tingna_sung@htc.com>
2014-10-21 17:09:57 +0800

    Measure absolute value of pointer motion distance
    If pointer is moving to the negative axis direction, pointer movement
    distance will always less than pre-defined mMotionSlop, thus
    TAP_OUTSIDE_STACK will have chance to be sent, will cause unnecessary
    focus stack switch.
    
    Change-Id: Ie204f69f232c771fad7318396843e8d55e83ac2c
    Signed-off-by: tingna_sung <tingna_sung@htc.com>

--- services/core/java/com/android/server/wm/StackTapPointerEventListener.java
+++ services/core/java/com/android/server/wm/StackTapPointerEventListener.java
@@ -62,8 +62,8 @@ public class StackTapPointerEventListener implements PointerEventListener {
 
                     if ((motionEvent.getEventTime() - motionEvent.getDownTime()) > TAP_TIMEOUT_MSEC
                             || index < 0
-                            || (motionEvent.getX(index) - mDownX) > mMotionSlop
-                            || (motionEvent.getY(index) - mDownY) > mMotionSlop) {
+                            || Math.abs(motionEvent.getX(index) - mDownX) > mMotionSlop
+                            || Math.abs(motionEvent.getY(index) - mDownY) > mMotionSlop) {
                         mPointerId = -1;
                     }
                 }
@@ -76,15 +76,13 @@ public class StackTapPointerEventListener implements PointerEventListener {
                 if (mPointerId == motionEvent.getPointerId(index)) {
                     final int x = (int)motionEvent.getX(index);
                     final int y = (int)motionEvent.getY(index);
-                    synchronized(this) {
-                        if ((motionEvent.getEventTime() - motionEvent.getDownTime())
-                                < TAP_TIMEOUT_MSEC
-                                && Math.abs(x - mDownX) < mMotionSlop
-                                && Math.abs(y - mDownY) < mMotionSlop
-                                && !mTouchExcludeRegion.contains(x, y)) {
-                            mService.mH.obtainMessage(H.TAP_OUTSIDE_STACK, x, y,
-                                    mDisplayContent).sendToTarget();
-                        }
+                    if ((motionEvent.getEventTime() - motionEvent.getDownTime())
+                            < TAP_TIMEOUT_MSEC
+                            && Math.abs(x - mDownX) < mMotionSlop
+                            && Math.abs(y - mDownY) < mMotionSlop
+                            && !mTouchExcludeRegion.contains(x, y)) {
+                        mService.mH.obtainMessage(H.TAP_OUTSIDE_STACK, x, y,
+                                mDisplayContent).sendToTarget();
                     }
                     mPointerId = -1;
                 }
