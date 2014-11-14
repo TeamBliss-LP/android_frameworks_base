640a62c97c1b4fe84eba12085e4ab3ad21592825
Darescu Ionut <ionut.darescu@gmail.com>
2014-11-14 04:24:58 -0500

    AdBlocker 2/3
    Change-Id: I4bf5f55021cf234e47cf344d0183b24b1550ca4e
    credit: tsubus

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2990,6 +2990,12 @@ public final class Settings {
         public static final String HARDWARE_KEYS_DISABLE = "hardware_keys_disable";
 
         /**
+         * Disable ads (HFM)
+         * @hide
+         */
+        public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
