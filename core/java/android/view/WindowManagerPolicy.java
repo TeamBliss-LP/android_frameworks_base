f5eaa60b6ea91a96e65562bc798b12a1afc44f8c
cristianomatos <cristianobmatos@gmail.com>
2014-12-03 11:55:59 -0200

    [Base] Nav bar tweaks
        - Toggle on/off (CyanogenMod alredy did but only for hw keys devices). crDroid allow all devices to do that for those using PIE apps.
        - Nav bar dimensions (height and width)
        - Here we also automatically reverts CyanogenMod navbar toggle: https://github.com/crdroidandroid/android_frameworks_base/commit/b70cda7d0d9b5142f54d390275b5e2ffa64ae9b4
    
        PS: THIS COMMIT IS A SQUASHED PORTED FROM crDroid KK and 99% od the codes are from SlimRoms. I want to say a HUGE thanks to @kufikugel
    
    Change-Id: I50a1c8d36efa3cf730bbb503fd20c3951af152ed

--- core/java/android/view/WindowManagerPolicy.java
+++ core/java/android/view/WindowManagerPolicy.java
@@ -1200,11 +1200,6 @@ public interface WindowManagerPolicy {
     public boolean hasPermanentMenuKey();
 
     /**
-     * Specifies whether the device needs a navigation bar (because it has no hardware buttons)
-     */
-    public boolean needsNavigationBar();
-
-    /**
      * Lock the device now.
      */
     public void lockNow(Bundle options);
