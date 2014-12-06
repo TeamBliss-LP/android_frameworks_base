4f1020b4ea80aede194704b21e005f0ad3192f14
Shaun Eaton <shaunsibu@gmail.com>
2014-12-06 14:13:15 -0700

    We don;t use CM DEV_FORCE_NAV_BAR
    Change-Id: I9aaec529fd4ba086ddb72e1ecdb30247667077aa

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2299,9 +2299,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             loadIntegerSetting(stmt, Settings.System.POINTER_SPEED,
                     R.integer.def_pointer_speed);
 
-            loadIntegerSetting(stmt, Settings.System.DEV_FORCE_SHOW_NAVBAR,
-                    R.integer.def_force_disable_navkeys);
-
             loadIntegerSetting(stmt, Settings.System.STATUS_BAR_NOTIF_COUNT,
                     R.integer.def_notif_count);
 
