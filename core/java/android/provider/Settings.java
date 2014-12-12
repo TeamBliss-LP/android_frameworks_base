83a876eb805d92526ef35a36a8cc11742e8a4416
Shaun Eaton <shaunsibu@gmail.com>
2014-12-12 02:41:07 -0700

    PORT- Bring in Battery Bar from Bliss Stalk. (1/2)
    Change-Id: Ida01d2db10569bb897eea452dabc8abeac002c45

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3649,6 +3649,32 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_NOTIF_COUNT = "status_bar_show_notif_count";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate"; 
+
+        /**
          * Visibility of SIM icons of unpopulated SIM slots in MSIM mode
          * 0: Hide unpopulated SIM icons
          * 1: Show unpopulated SIM icons
