e84d73d2261dfc9c614dd6225f37a39993bfa466
Shaun Eaton <shaunsibu@gmail.com>
2014-12-15 14:26:11 -0700

    We are Bliss and want to keep it clean.
    Change-Id: I92d68fa680c74dc10409741e308149679c27749c

--- core/java/com/android/internal/util/bliss/AwesomeAnimationHelper.java
+++ core/java/com/android/internal/util/bliss/AwesomeAnimationHelper.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.util.validus;
+package com.android.internal.util.bliss;
 
 import android.content.Context;
 import android.content.res.Resources;
