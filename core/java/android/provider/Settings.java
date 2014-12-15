a9c54156c095f3658f59f237507b54bfc5e73cda
Shaun Eaton <shaunsibu@gmail.com>
2014-12-15 08:54:00 -0700

    PORT-Custom scrolling values 1/2 from Bliss Stalk.
    Change-Id: Ia8efce0d2172e5cfe82a4b27b06f060fa89a3b73

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2601,6 +2601,30 @@ public final class Settings {
          * @hide
          */
         public static final String USE_EDGE_SERVICE_FOR_GESTURES = "edge_service_for_gestures";
+        
+        /**
+         * Determine custom scroll friction.
+         * @hide
+         */
+        public static final String CUSTOM_SCROLL_FRICTION = "custom_scroll_friction";
+
+        /**
+         * Determine custom fling velocity.
+         * @hide
+         */
+        public static final String CUSTOM_FLING_VELOCITY = "custom_fling_velocity";
+
+        /**
+         * Determine custom overscroll distance.
+         * @hide
+         */
+        public static final String CUSTOM_OVERSCROLL_DISTANCE = "custom_overscroll_distance";
+
+        /**
+         * Determine custom overfling distance.
+         * @hide
+         */
+        public static final String CUSTOM_OVERFLING_DISTANCE = "custom_overfling_distance";        
 
         /**
          * Show the pending notification counts as overlays on the status bar
