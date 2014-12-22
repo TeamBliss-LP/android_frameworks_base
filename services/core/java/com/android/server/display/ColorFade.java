b109a45a616e048c757243ac9a5567c3b1d06814
Evisceration <eviscerationls@gmail.com>
2014-12-22 23:46:58 +0100

    services: fix memory leak in ColorFade
    original patch from
       https://code.google.com/p/android/issues/detail?id=79729#c177
    
    i can not get the email to add proper authorship, but all credits belong
    to this person
    
    Change-Id: Ica9a4ae02cd3c8435f46dd9c3bfdb52317f9a7ef
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- services/core/java/com/android/server/display/ColorFade.java
+++ services/core/java/com/android/server/display/ColorFade.java
@@ -449,6 +449,7 @@ final class ColorFade {
         if (!attachEglContext()) {
             return false;
         }
+        SurfaceTexture st = null;
         try {
             if (!mTexNamesGenerated) {
                 GLES20.glGenTextures(1, mTexNames, 0);
@@ -458,7 +459,7 @@ final class ColorFade {
                 mTexNamesGenerated = true;
             }
 
-            final SurfaceTexture st = new SurfaceTexture(mTexNames[0]);
+            st = new SurfaceTexture(mTexNames[0]);
             final Surface s = new Surface(st);
             try {
                 SurfaceControl.screenshot(SurfaceControl.getBuiltInDisplay(
@@ -482,6 +483,7 @@ final class ColorFade {
             GLES20.glViewport(0, 0, mDisplayWidth, mDisplayHeight);
             ortho(0, mDisplayWidth, 0, mDisplayHeight, -1, 1);
         } finally {
+            if (st != null) st.release();
             detachEglContext();
         }
         return true;
