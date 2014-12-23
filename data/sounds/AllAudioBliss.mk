648dccf3565914709e5d824601aeec17ed869ad6
Shaun Eaton <shaunsibu@gmail.com>
2014-12-23 10:39:42 -0700

    Blissify
    Change-Id: Ie7325c5b5fb1c4a33437aa1f4d2461df2cbd10b9

--- data/sounds/AllAudioBliss.mk
+++ data/sounds/AllAudioBliss.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := frameworks/base/data/sounds/slim
+LOCAL_PATH := frameworks/base/data/sounds/bliss
 
 define create-copy-media-files
 $(strip $(foreach fp,\
