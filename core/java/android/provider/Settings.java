e449a14f7c449b1654edc8dfd0a5272a4d7e0f1d
cristianomatos <cristianobmatos@gmail.com>
2014-12-31 10:33:44 -0200

    AOKP system animations: remove no app override
    - Removed until we find a better solution regarding recents animations
    
    Change-Id: I1bdafa83fb8dcbc48228418fd4108a674b176135

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3656,9 +3656,7 @@ public final class Settings {
                 "task_open_behind",
         };  
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
-        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
-
-
+        
         /**
          * ListView Animations
          * 0 == None
