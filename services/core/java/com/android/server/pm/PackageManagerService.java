ffd20265fc43305126d460721cae349de3ceaed2
Shaun Eaton <shaunsibu@gmail.com>
2014-12-30 21:45:47 -0700

    Revert "Show package names during dexopt"
    This reverts commit e69273e492c1a60eadb1a5bdb1bdd5d54ea7f526.

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4697,25 +4697,13 @@ public class PackageManagerService extends IPackageManager.Stub {
             if (lowThreshold == 0) {
                 throw new IllegalStateException("Invalid low memory threshold");
             }
-
-            PackageManager pm = null;;
-            if (mContext != null)
-                pm = mContext.getPackageManager();
-            String n = null;
-
             for (PackageParser.Package pkg : sortedPkgs) {
                 long usableSpace = dataDir.getUsableSpace();
                 if (usableSpace < lowThreshold) {
                     Log.w(TAG, "Not running dexopt on remaining apps due to low memory: " + usableSpace);
                     break;
                 }
-                if (pm != null)
-                    n = (String)pkg.applicationInfo.loadLabel(pm);
-                if (n == null || n.length() == 0)
-                    n = pkg.packageName;
-                final String name = "\n"+n;
-                n = null;
-                performBootDexOpt(pkg, ++i, total, name);
+                performBootDexOpt(pkg, ++i, total);
             }
         }
     }
@@ -4773,10 +4761,6 @@ public class PackageManagerService extends IPackageManager.Stub {
     }
 
     private void performBootDexOpt(PackageParser.Package pkg, int curr, int total) {
-        performBootDexOpt(pkg, curr, total, "");
-    }
-
-    private void performBootDexOpt(PackageParser.Package pkg, int curr, int total, String pkgname) {
         if (DEBUG_DEXOPT) {
             Log.i(TAG, "Optimizing app " + curr + " of " + total + ": " + pkg.packageName);
         }
@@ -4784,7 +4768,7 @@ public class PackageManagerService extends IPackageManager.Stub {
             try {
                 ActivityManagerNative.getDefault().showBootMessage(
                         mContext.getResources().getString(R.string.android_upgrading_apk,
-                                curr, total, pkgname), true);
+                                curr, total), true);
             } catch (RemoteException e) {
             }
         }
