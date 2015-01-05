c8014cdf1a3429a312de6fb2fd4681baa8c532c8
Shaun Eaton <shaunsibu@gmail.com>
2015-01-05 12:43:46 -0700

    Changes to hopefully fix search bar in recents.
    Change-Id: I68f47e1679667c58152b77d72359e8ccac43c030

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -278,35 +278,33 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
             }
             findViewById(R.id.floating_action_button).setVisibility(View.VISIBLE);
             if (mRecentsView.hasSearchBar()) {
-
                 if (Settings.System.getInt(getContentResolver(),
-                    Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR, 1) == 0) {
+                    Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR, 0) == 1) {
                     mRecentsView.setSearchBarVisibility(View.VISIBLE);
                 } else {
                     mRecentsView.setSearchBarVisibility(View.GONE);
                    }
                 } else {
                 if (Settings.System.getInt(getContentResolver(),
-                    Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR, 1) == 0) {
+                    Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR, 0) == 1) {
                     addSearchBarAppWidgetView();
             } else {
-               }
                 }
+            }
         }
 
-	// Update search bar space height
+        // Update search bar space height
         Resources reso = getResources();
 
         if (Settings.System.getInt(getContentResolver(),
                     Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR, 0) != 1) {
         RecentsConfiguration.searchBarSpaceHeightPx = 0;
-
-		}
+	}
 
         if (Settings.System.getInt(getContentResolver(),
                     Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR, 1) != 0) {
 	RecentsConfiguration.searchBarSpaceHeightPx = reso.getDimensionPixelSize(R.dimen.recents_search_bar_space_height);
-		}
+	}
 
         // Animate the SystemUI scrims into view
         mScrimViews.prepareEnterRecentsAnimation();
@@ -374,6 +372,8 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
                     // Save the app widget id into the settings
                     mConfig.updateSearchBarAppWidgetId(this, widgetInfo.first);
                     mSearchAppWidgetInfo = widgetInfo.second;
+                } else {
+                    mConfig.updateSearchBarAppWidgetId(this, -1);
                 }
             }
         }
