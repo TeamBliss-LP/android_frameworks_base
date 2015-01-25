ff486f0d091fd7cb66a672a15515a983dda94625
Evan Anderson <evananderson@aospa.co>
2015-01-25 15:54:40 -0500

    GlobalActions: Confirm power off
    In stock lollipop power off is the only action in the GlobalActions menu.
    Since we added more actions we should confirm the power off command in
    case the user accidentally tapped the wrong item.
    
    Change-Id: I5b126aa47956abbbe26417f5c59d8a181c70dee2

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -536,7 +536,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 return;
             }
             // shutdown by making sure radio and power are handled accordingly.
-            mWindowManagerFuncs.shutdown(false /* confirm */);
+            mWindowManagerFuncs.shutdown(true /* confirm */);
         }
     }
 
