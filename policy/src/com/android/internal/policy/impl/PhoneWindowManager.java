8260ef6af6fae313cb5774574ac281424d1c5e66
Ryan <ryantl66@gmail.com>
2015-01-27 02:31:34 -0500

    remove double vibrate
    Change-Id: I953ba826bcdf9ef3250742dfaec620923cc97e42

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5483,7 +5483,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         if (useHapticFeedback) {
-            performHapticFeedbackLw(null, HapticFeedbackConstants.VIRTUAL_KEY, false);
+            //performHapticFeedbackLw(null, HapticFeedbackConstants.VIRTUAL_KEY, false);
         }
 
         if (isWakeKey) {
