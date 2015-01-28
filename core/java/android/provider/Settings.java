61c899968bca33a16ca71613472bda9f780b593a
Shaun Eaton <shaunsibu@gmail.com>
2015-01-28 12:32:13 -0700

    Needed Settings lines for Breathing Notifications.
    Change-Id: I384898efa972bcc5fef24b0fd2bcf70561cb3da5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3974,6 +3974,24 @@ public final class Settings {
          * @hide
          */
         public static final String VOICE_LAUNCH_INTENT = "voice_launch_intent";
+        
+        /**
+	     * Give SMS notifications a breathing effect
+         * @hide
+	     */
+        public static final String KEY_SMS_BREATH = "sms_breath";
+
+        /**
+	     * Give Missed call notifications a breathing effect
+	     * @hide
+	     */
+        public static final String KEY_MISSED_CALL_BREATH = "missed_call_breath";
+
+        /**
+	     * Give Voicemail notifications a breathing effect
+	     * @hide
+	     */
+        public static final String KEY_VOICEMAIL_BREATH = "voicemail_breath";        
 
         /**
          * ListView Animations
