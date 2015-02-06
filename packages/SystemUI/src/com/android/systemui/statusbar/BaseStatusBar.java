8f0543ee662f2512c89bc18daa454698a8e50569
BigBrother1984 <carlosavignano@aospa.co>
2015-02-06 08:37:15 -0200

    [Heads up] Don't show heads up while IME is showing
    Change-Id: I864ebea224d92077c1f1720dd7ff25cf08d83662

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -84,6 +84,7 @@ import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
 import android.view.accessibility.AccessibilityManager;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.DateTimeView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -2238,8 +2239,9 @@ public abstract class BaseStatusBar extends SystemUI implements
                 && isAllowed
                 && !accessibilityForcesLaunch
                 && mPowerManager.isScreenOn()
-                && !keyguardIsShowing
                 && !isExpanded;
+                && !keyguardIsShowing
+                && !isImeShowing();;
 
         try {
             interrupt = interrupt && !mDreamManager.isDreaming();
@@ -2289,6 +2291,16 @@ public abstract class BaseStatusBar extends SystemUI implements
                 }
             }
         }
+    }
+
+    /**
+     * @return Whether IME input is showing.
+     */
+    public boolean isImeShowing() {
+        final InputMethodManager inputMethodManager = (InputMethodManager)
+                mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        return inputMethodManager != null ? inputMethodManager.isImeShowing() : false;
     }
 
     public boolean inKeyguardRestrictedInputMode() {
