d8e9d2b6d25f7edae9c8a2a576c88ef1428101f8
Carlo Conserva <carlo.conserva@gmail.com>
2015-02-09 23:00:01 +0000

    SystemUI: fix profiles tile disappearing after profile selection.
    This should fix a race condition between the modified method and
    the onStateChanged callback method (lines 278-287 of the same file)
    that is causing the profiles tile to disappear most of the times
    after selecting a new profile and pressing "Done" in the detail view.
    
    Change-Id: I40074cd8247879c843cd24e3749075876750d519

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -445,6 +445,7 @@ public class QSPanel extends ViewGroup {
     }
 
     private void setGridContentVisibility(boolean visible) {
+        mGridContentVisible = visible;
         int newVis = visible ? VISIBLE : INVISIBLE;
         for (int i = 0; i < mRecords.size(); i++) {
             TileRecord tileRecord = mRecords.get(i);
@@ -453,7 +454,6 @@ public class QSPanel extends ViewGroup {
             }
         }
         mBrightnessView.setVisibility(showBrightnessSlider() ? newVis : GONE);
-        mGridContentVisible = visible;
     }
 
     @Override
