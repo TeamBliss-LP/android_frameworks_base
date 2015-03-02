sub77-bkp/frameworks_base

Action: Add kill-app and last-app actions
Forward port from KitKat

Change-Id: If8326ffed21faa2452e22b2afb717f4c123bd2a0

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -61,6 +61,8 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_START_CUSTOM_INTENT_AFTER_KEYGUARD = 19 << MSG_SHIFT;
     private static final int MSG_HIDE_HEADS_UP_CANDIDATE            = 20 << MSG_SHIFT;
     private static final int MSG_HIDE_HEADS_UP                      = 21 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_LAST_APP                    = 22 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_KILL_APP                    = 23 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -106,6 +108,8 @@ public class CommandQueue extends IStatusBar.Stub {
         public void showCustomIntentAfterKeyguard(Intent intent);
         public void hideHeadsUpCandidate(String packageName);
         public void scheduleHeadsUpClose();
+        public void toggleLastApp();
+        public void toggleKillApp();
     }
 
     public CommandQueue(Callbacks callbacks, StatusBarIconList list) {
@@ -279,6 +283,20 @@ public class CommandQueue extends IStatusBar.Stub {
         }
     }
 
+    public void toggleLastApp() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_TOGGLE_LAST_APP);
+            mHandler.obtainMessage(MSG_TOGGLE_LAST_APP, 0, 0, null).sendToTarget();
+        }
+    }
+
+    public void toggleKillApp() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_TOGGLE_KILL_APP);
+            mHandler.obtainMessage(MSG_TOGGLE_KILL_APP, 0, 0, null).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         public void handleMessage(Message msg) {
             final int what = msg.what & MSG_MASK;
@@ -369,6 +387,12 @@ public class CommandQueue extends IStatusBar.Stub {
                     break;
                 case MSG_HIDE_HEADS_UP:
                     mCallbacks.scheduleHeadsUpClose();
+                    break;
+                case MSG_TOGGLE_LAST_APP:
+                    mCallbacks.toggleLastApp();
+                    break;
+                case MSG_TOGGLE_KILL_APP:
+                    mCallbacks.toggleKillApp();
                     break;
             }
         }

TeamBliss-LP/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -60,6 +60,8 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_SET_AUTOROTATE_STATUS              = 18 << MSG_SHIFT;
     private static final int MSG_HIDE_HEADS_UP                      = 19 << MSG_SHIFT;	
     private static final int MSG_START_CUSTOM_INTENT_AFTER_KEYGUARD = 20 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_LAST_APP                    = 21 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_KILL_APP                    = 22 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -105,6 +107,8 @@ public class CommandQueue extends IStatusBar.Stub {
         public void setAutoRotate(boolean enabled);
         public void showCustomIntentAfterKeyguard(Intent intent);
         public void scheduleHeadsUpClose();
+        public void toggleLastApp();
+        public void toggleKillApp();
     }
 
     public CommandQueue(Callbacks callbacks, StatusBarIconList list) {
@@ -278,6 +282,20 @@ public class CommandQueue extends IStatusBar.Stub {
         }
     }
 
+    public void toggleLastApp() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_TOGGLE_LAST_APP);
+            mHandler.obtainMessage(MSG_TOGGLE_LAST_APP, 0, 0, null).sendToTarget();
+        }
+    }
+
+    public void toggleKillApp() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_TOGGLE_KILL_APP);
+            mHandler.obtainMessage(MSG_TOGGLE_KILL_APP, 0, 0, null).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         public void handleMessage(Message msg) {
             if (mPaused) {
@@ -369,6 +387,12 @@ public class CommandQueue extends IStatusBar.Stub {
                     break;					
                 case MSG_START_CUSTOM_INTENT_AFTER_KEYGUARD:
                     mCallbacks.showCustomIntentAfterKeyguard((Intent) msg.obj);
+                    break;
+                case MSG_TOGGLE_LAST_APP:
+                    mCallbacks.toggleLastApp();
+                    break;
+                case MSG_TOGGLE_KILL_APP:
+                    mCallbacks.toggleKillApp();
                     break;
             }
         }