dfc6c9c860a1ed76e254fa4557f7f7a001713bc9
Danny Baumann <dannybaumann@web.de>
2015-03-03 13:45:28 +0530

    Hide unused msim icons on setting preference(2/2)
    Added a listPreference through which no-sim icons can be hidden or displayed
    as per the user's wish
    
    Change-Id: Ice8f6150a9b969bd6b36f4d0f45746467b271a40

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1124,7 +1124,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         if (isMSim()) {
             if (mMSimNetworkController == null) {
-                mMSimNetworkController = new MSimNetworkControllerImpl(mContext);
+                mMSimNetworkController = new MSimNetworkControllerImpl(mContext, mHandler);
             }
             MSimSignalClusterView signalCluster = (MSimSignalClusterView)
                     mStatusBarView.findViewById(R.id.msim_signal_cluster);
@@ -3910,6 +3910,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private void setControllerUsers() {
         if (mZenModeController != null) {
             mZenModeController.setUserId(mCurrentUserId);
+        }
+        if (mMSimNetworkController != null) {
+            mMSimNetworkController.setUserId(mCurrentUserId);
         }
     }
 
