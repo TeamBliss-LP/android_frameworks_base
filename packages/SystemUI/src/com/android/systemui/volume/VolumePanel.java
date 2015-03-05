aae566ba0d6ac63b82b27517819405c5f4a39bce
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-05 19:48:44 +0000

    Base: fix default stream controlled by volume rocker for tablets (1/2)
    Also
    * add STREAM_NOTIFICATION
    * make zen panel always available
    
    Change-Id: I30538d57ada8452d80614c369f992db89e3418cd

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -1013,7 +1013,9 @@ public class VolumePanel extends Handler {
     }
 
     private void updateZenPanelVisible() {
-        setZenPanelVisible(mZenModeAvailable && isNotificationOrRing(mActiveStreamType));
+        if (mZenModeAvailable) {
+            setZenPanelVisible(true);
+        }
     }
 
     public void postVolumeChanged(int streamType, int flags) {
