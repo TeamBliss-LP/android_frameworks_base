b6501a390109eb7ca7615c52301745a0cded0571
Roman Birg <roman@cyngn.com>
2015-03-04 18:33:24 -0800

    SystemUI: improve qs tile scroll handling
      - place detail view within bounds of expanded panel when it
       goes beyond the bottom of the screen
    
      - collapse qs panel when scrolling detail view (and letting go),
        disallowing it to get stuck at weird states that cut off the detail view
    
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    
    Change-Id: I22a9fd9b540462d6c092f213582075d9145261f2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -222,6 +222,16 @@ public class NotificationPanelView extends PanelView implements
         mKeyguardStatusView = (KeyguardStatusView) findViewById(R.id.keyguard_status_view);
         mQsContainer = findViewById(R.id.quick_settings_container);
         mQsPanel = (QSPanel) findViewById(R.id.quick_settings_panel);
+        mQsPanel.setDetailCallback(new QSPanel.DetailCallback() {
+            @Override
+            public void onDetailChanged(boolean showing) {
+                mQsPanel.setTopOfContainer(mQsContainer.getTop());
+                mQsPanel.setDetailOffset(mScrollView.getScrollY());
+                if (!showing) {
+                    mHandler.removeCallbacks(mCloseQsRunnable);
+                }
+            }
+        });
         mClockView = (TextView) findViewById(R.id.clock_view);
         mScrollView = (ObservableScrollView) findViewById(R.id.scroll_view);
         mScrollView.setListener(this);
@@ -607,6 +617,9 @@ public class NotificationPanelView extends PanelView implements
     }
 
     private boolean flingExpandsQs(float vel) {
+        if (isQsDetailShowing() && mStatusBarState == StatusBarState.SHADE) {
+            return false;
+        }
         if (isBelowFalsingThreshold()) {
             return false;
         }
@@ -728,6 +741,9 @@ public class NotificationPanelView extends PanelView implements
         // If we are already running a QS expansion, make sure that we keep the panel open.
         if (mQsExpansionAnimator != null) {
             expands = true;
+        }
+        if (isQsDetailShowing()) {
+            expands = false;
         }
         return expands;
     }
@@ -965,7 +981,7 @@ public class NotificationPanelView extends PanelView implements
             return true;
         }
     };
-    
+
     private void animateHeaderSlidingIn() {
         mHeaderAnimatingIn = true;
         getViewTreeObserver().addOnPreDrawListener(mStartHeaderSlidingIn);
@@ -1593,9 +1609,30 @@ public class NotificationPanelView extends PanelView implements
     public void onReset(ExpandableView view) {
     }
 
+    private Runnable mCloseQsRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (isQsDetailShowing()
+                    && !mTracking
+                    && (!mQsTracking || mStatusBarState == StatusBarState.KEYGUARD)
+                    && !mIntercepting
+                    && mQsExpansionAnimator == null) {
+                if (mStatusBarState == StatusBarState.SHADE) {
+                    closeQsDetail();
+                }
+                animateCloseQs();
+            }
+        }
+    };
+
     @Override
     public void onScrollChanged() {
+        mQsPanel.setDetailOffset(mScrollView.getScrollY());
         if (mQsExpanded) {
+            if (isQsDetailShowing()) {
+                mHandler.removeCallbacks(mCloseQsRunnable);
+                mHandler.postDelayed(mCloseQsRunnable, 200);
+            }
             requestScrollerTopPaddingUpdate(false /* animate */);
             requestPanelHeightUpdate();
         }
