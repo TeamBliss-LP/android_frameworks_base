46fdda7afc54df4670f8721c1613893c8921d67f
d34d <clark@cyngn.com>
2015-03-11 11:23:15 -0700

    Themes: Process theme resources after package scanned [1/2]
    This patch processes a theme's resources after the package is
    scanned and the theme is added to the PackageManager.
    
    Change-Id: I5e76e177b355853ea5cf2e1a5655863dccf3b484

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -1152,6 +1152,10 @@ public class PackageManagerService extends IPackageManager.Stub {
                                             pkgList,uidArray, null);
                                 }
                             }
+                            // if this was a theme, send it off to the theme service for processing
+                            if(res.pkg.mIsThemeApk || res.pkg.mIsLegacyIconPackApk) {
+                                processThemeResourcesInThemeService(res.pkg.packageName);
+                            }
                             if (res.removedInfo.args != null) {
                                 // Remove the replaced package's older resources safely now
                                 deleteOld = true;
@@ -6690,15 +6694,6 @@ public class PackageManagerService extends IPackageManager.Stub {
                         throw new PackageManagerException(failReason,
                                 "Unable to process theme " + pkgName, failedException);
                     }
-                }
-            }
-
-            if (!isBootScan && (pkg.mIsThemeApk)) {
-                // Pass this off to the ThemeService for processing
-                ThemeManager tm =
-                        (ThemeManager) mContext.getSystemService(Context.THEME_SERVICE);
-                if (tm != null) {
-                    tm.processThemeResources(pkg.packageName);
                 }
             }
 
@@ -14503,6 +14498,14 @@ public class PackageManagerService extends IPackageManager.Stub {
         }
 
         return 0;
+    }
+
+    private void processThemeResourcesInThemeService(String pkgName) {
+        ThemeManager tm =
+                (ThemeManager) mContext.getSystemService(Context.THEME_SERVICE);
+        if (tm != null) {
+            tm.processThemeResources(pkgName);
+        }
     }
 
     /**
