2b9164ee971b487a59107c4497b13dc291ece31c
sethyx <sethyx@gmail.com>
2015-03-31 22:32:38 +0200

    [1/3] frameworks/base: Add NFC polling setting
    Change-Id: I29b4a49d846da3b8eebf6c92cc3745f035034603

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4389,6 +4389,11 @@ public final class Settings {
         public static final String VOLUME_KEYS_DEFAULT = "volume_keys_default";
 
         /**
+         * Define when Nfc polling is activated.
+         */
+        public static final String NFC_POLLING_MODE = "nfc_polling";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
