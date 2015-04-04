288a356cefcfd57dfafdff33df55e3f00a13d962
LorDClockaN <davor@losinj.com>
2015-04-04 16:35:46 +0200

    Clean LS colors code a little
    comment back what was commented in 5.1
    
    Change-Id: Ibb04f474aa560877b1f90583f3586bb4d7376da4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -574,6 +574,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
             mLockIcon.setImageDrawable(icon);
             mLockIcon.updateColorSettings();
         }*/
+        mLockIcon.updateColorSettings();
         boolean trustManaged = mUnlockMethodCache.isTrustManaged();
         mTrustDrawable.setTrustManaged(trustManaged);
         updateLockIconClickability();
