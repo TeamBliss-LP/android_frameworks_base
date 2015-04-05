2b1b6fd2f5081da434de1258dd68b5611eff2633
Janson Kang <temasek71@gmail.com>
2015-04-05 23:51:18 +0800

    BaseStatusBar: Heads up fixes
    Change-Id: Iafb4b5596ad0a38cac3ccd1f955450e82fba78e1

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2379,7 +2379,6 @@ public abstract class BaseStatusBar extends SystemUI implements
                 && (!mStatusBarKeyguardViewManager.isShowing()
                         || mStatusBarKeyguardViewManager.isOccluded())
                 && !mStatusBarKeyguardViewManager.isInputRestricted()
-                && !keyguardIsShowing
                 && !isExpanded
                 && !isImeShowing();
         try {
@@ -2391,7 +2390,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         // its below our threshold priority, we might want to always display
         // notifications from certain apps
         if (!isHighPriority && !isOngoing && !isExpanded
-                    && !zenBlocksHeadsUp && !isIMEShowing) {
+                    && !zenBlocksHeadsUp && !isExpanded) {
             // However, we don't want to interrupt if we're in an application that is
             // in Do Not Disturb
             if (!isPackageInDnd(getTopLevelPackage())) {
