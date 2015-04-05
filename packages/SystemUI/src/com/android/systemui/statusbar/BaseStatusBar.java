200c2e4b7601e6dbb4be14e3feacdf7690b19db6
abun880007 <tigger2014@team-ub.co.uk>
2015-04-05 18:48:14 +0100

    Fix showing heads up if IME is showing
    Change-Id: Id7bda8eefd85ed2e904fe75fcf9bf3421c3f7e0b

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2390,7 +2390,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         // its below our threshold priority, we might want to always display
         // notifications from certain apps
         if (!isHighPriority && !isOngoing && !isExpanded
-                    && !zenBlocksHeadsUp && !isExpanded) {
+                    && !zenBlocksHeadsUp && !isImeShowing()) {
             // However, we don't want to interrupt if we're in an application that is
             // in Do Not Disturb
             if (!isPackageInDnd(getTopLevelPackage())) {
