a45bb0704cbf1a37902c0a8ffbfa412bd1c61ef5
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-04-20 09:36:41 -0600

    Ticker custom text color -Also some fixes to ticker from lp5.0
    Change-Id: Iac34b7dfce93b14d1f897409e1b8e128ad0a2770

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2325,11 +2325,10 @@ public final class Settings {
         public static final String QS_COLOR_SWITCH = "qs_color_switch";
 
         /**
-         * Whether to enable ticker animation in statusbar
-         *
+         * Wether to show the ticker on the status bar
          * @hide
          */
-        public static final String STATUS_BAR_TICKER_ENABLED = "status_bar_ticker_enabled";
+        public static final String STATUS_BAR_SHOW_TICKER = "status_bar_show_show_ticker";
 
         /**
          * Control whether the process CPU usage meter should be shown.
