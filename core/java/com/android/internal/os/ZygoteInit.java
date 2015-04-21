dfe154623ecd15bbd4cc6d6f1369a2bb504f015c
Andreas Gampe <agampe@google.com>
2015-04-20 19:16:37 -0700

    Frameworks/base: Use better Class.forName in ZygoteInit
    Use the three-argument version of Class.forName. This saves an
    expensive stack lookup for the caller's class-loader on each
    invocation.
    
    Bug: 19498458
    
    * CP from here:
    https://github.com/android/platform_frameworks_base/commit/c917f74d9235feefd1788a7b9ba34ed8f1215850
    
    Change-Id: I5c0559fa6015ce80915d60e016e814726c984b5d

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -325,19 +325,12 @@ public class ZygoteInit {
                 }
 
                 try {
-                    if (false) {
-                        Log.v(TAG, "Preloading " + line + "...");
-                    }
-                    Class.forName(line);
-                    if (Debug.getGlobalAllocSize() > PRELOAD_GC_THRESHOLD) {
-                        if (false) {
-                            Log.v(TAG,
-                                " GC at " + Debug.getGlobalAllocSize());
-                        }
-                        System.gc();
-                        runtime.runFinalizationSync();
-                        Debug.resetGlobalAllocSize();
-                    }
+                    // Load and explicitly initialize the given class. Use the tree-argument version
+                    // of forName to avoid repeated stack lookups (to derive the caller's
+                    // class-loader). Use true to force initialization, and null for the boot
+                    // classpath class-loader (could as well cache the class-loader of this class in
+                    // a variable).
+                    Class.forName(line, true, null);
                     count++;
                 } catch (ClassNotFoundException e) {
                     Log.w(TAG, "Class not found for preloading: " + line);
