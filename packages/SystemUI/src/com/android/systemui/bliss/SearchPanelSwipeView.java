AICP/frameworks_base

Make Navring available to devices with no navbar
- Modified to work with our navbar ring customization

Change-Id: I17eae46576c0e397b096ecf1dd64c2ac1bb30001

packages/SystemUI/src/com/android/systemui/aicp/SearchPanelSwipeView.java
@@ -1,0 +1,120 @@
+package com.android.systemui.aicp;
+
+import com.android.systemui.SearchPanelView;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.BaseStatusBar;
+import com.android.systemui.statusbar.DelegateViewHelper;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class SearchPanelSwipeView extends LinearLayout{
+
+    private Context mContext;
+    private Resources res;
+    private int mButtonHeight = 50;
+    private int mGestureHeight;
+    private ImageView mDragButton;
+    private DelegateViewHelper mDelegateHelper;
+
+    public SearchPanelSwipeView(Context context, BaseStatusBar bar) {
+        super(context);
+        mContext = context;
+        mDelegateHelper = new DelegateViewHelper(this);
+        setBar(bar);
+        mDragButton = new ImageView(mContext);
+        res = mContext.getResources();
+        mGestureHeight = res.getDimensionPixelSize(R.dimen.ribbon_drag_handle_height);
+        updateLayout();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        setIntialTouchArea();
+        if (mDelegateHelper != null) {
+            boolean ret = mDelegateHelper.onInterceptTouchEvent(event);
+            if (ret) return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        setIntialTouchArea();
+        return mDelegateHelper.onInterceptTouchEvent(event);
+    }
+
+    public void setIntialTouchArea() {
+        mDelegateHelper.setInitialTouchRegion(this);
+    }
+
+    public void setDelegateView(SearchPanelView searchPanelView) {
+        mDelegateHelper.setDelegateView(searchPanelView);
+    }
+
+    public void setBar(BaseStatusBar bar) {
+        mDelegateHelper.setBar(bar);
+    }
+
+    private int getGravity() {
+        if (isScreenPortrait()) {
+            return Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+        } else {
+            return Gravity.CENTER_VERTICAL | Gravity.RIGHT;
+        }
+    }
+
+    public WindowManager.LayoutParams getGesturePanelLayoutParams() {
+        WindowManager.LayoutParams lp;
+        if (isScreenPortrait()) {
+            lp  = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                    | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                    PixelFormat.TRANSLUCENT);
+        } else {
+            lp  = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.WRAP_CONTENT,WindowManager.LayoutParams.MATCH_PARENT,
+                    WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                    | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                    PixelFormat.TRANSLUCENT);
+        }
+        lp.gravity = getGravity();
+        lp.setTitle("SwipePanelSwipeView");
+        return lp;
+    }
+
+    public void updateLayout() {
+        LinearLayout.LayoutParams dragParams;
+        float dragHeight = (mGestureHeight * (mButtonHeight * 0.01f));
+        removeAllViews();
+        mDragButton.setBackgroundColor(Color.BLACK);
+        if (isScreenPortrait()) {
+            mDelegateHelper.setSwapXY(false);
+            dragParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (int) dragHeight);
+        } else {
+            mDelegateHelper.setSwapXY(true);
+            dragParams = new LinearLayout.LayoutParams((int) dragHeight, LinearLayout.LayoutParams.MATCH_PARENT);
+        }
+        mDragButton.setVisibility(View.INVISIBLE);
+        addView(mDragButton,dragParams);
+        invalidate();
+    }
+
+    public boolean isScreenPortrait() {
+        return res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -144,6 +144,7 @@ import com.android.internal.util.cm.WeatherControllerImpl;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.BatteryMeterView;
+import com.android.systemui.aicp.SearchPanelSwipeView;
 import com.android.systemui.BatteryLevelTextView;
 import com.android.systemui.DemoMode;
 import com.android.systemui.EventLogConstants;
@@ -700,6 +701,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private ScrimController mScrimController;
     private DozeScrimController mDozeScrimController;
 
+    private SearchPanelSwipeView mSearchPanelSwipeView;
+
     private final Runnable mAutohide = new Runnable() {
         @Override
         public void run() {
@@ -1042,6 +1045,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         checkUserAutohide(v, event);
                         return false;
                     }});
+            }
+        } catch (RemoteException ex) {
+            // no window manager? good luck with that
+        }
+
+        try {
+            boolean showNav = mWindowManagerService.hasNavigationBar();
+            if (!showNav) {
+                mSearchPanelSwipeView = new SearchPanelSwipeView(mContext, this);
+                mWindowManager.addView(mSearchPanelSwipeView, mSearchPanelSwipeView.getGesturePanelLayoutParams());
+                updateSearchPanel();
             }
         } catch (RemoteException ex) {
             // no window manager? good luck with that
@@ -1497,6 +1511,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mNavigationBarView != null) {
             mNavigationBarView.setDelegateView(mSearchPanelView);
         }
+        if (mSearchPanelSwipeView != null) {
+            mSearchPanelSwipeView.setDelegateView(mSearchPanelView);
+        }
     }
 
     @Override
@@ -1637,6 +1654,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         prepareNavigationBarView(false);
 
         mWindowManager.updateViewLayout(mNavigationBarView, getNavigationBarLayoutParams());
+    }
+
+    private void repositionSearchPanelSwipeView() {
+        if (mSearchPanelSwipeView == null || !mSearchPanelSwipeView.isAttachedToWindow()) return;
+        mSearchPanelSwipeView.updateLayout();
+        mWindowManager.updateViewLayout(mSearchPanelSwipeView, mSearchPanelSwipeView.getGesturePanelLayoutParams());
+        updateSearchPanel();
     }
 
     private void notifyNavigationBarScreenOn(boolean screenOn) {
@@ -3899,6 +3923,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
             else if (Intent.ACTION_SCREEN_ON.equals(action)) {
                 mScreenOn = true;
+                repositionSearchPanelSwipeView();
                 notifyNavigationBarScreenOn(true);
             }
             else if (ACTION_DEMO.equals(action)) {

TeamBliss-LP/android_frameworks_base

packages/SystemUI/src/com/android/systemui/bliss/SearchPanelSwipeView.java
@@ -1,0 +1,120 @@
+package com.android.systemui.bliss;
+
+import com.android.systemui.SearchPanelView;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.BaseStatusBar;
+import com.android.systemui.statusbar.DelegateViewHelper;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class SearchPanelSwipeView extends LinearLayout{
+
+    private Context mContext;
+    private Resources res;
+    private int mButtonHeight = 50;
+    private int mGestureHeight;
+    private ImageView mDragButton;
+    private DelegateViewHelper mDelegateHelper;
+
+    public SearchPanelSwipeView(Context context, BaseStatusBar bar) {
+        super(context);
+        mContext = context;
+        mDelegateHelper = new DelegateViewHelper(this);
+        setBar(bar);
+        mDragButton = new ImageView(mContext);
+        res = mContext.getResources();
+        mGestureHeight = res.getDimensionPixelSize(R.dimen.ribbon_drag_handle_height);
+        updateLayout();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        setIntialTouchArea();
+        if (mDelegateHelper != null) {
+            boolean ret = mDelegateHelper.onInterceptTouchEvent(event);
+            if (ret) return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        setIntialTouchArea();
+        return mDelegateHelper.onInterceptTouchEvent(event);
+    }
+
+    public void setIntialTouchArea() {
+        mDelegateHelper.setInitialTouchRegion(this);
+    }
+
+    public void setDelegateView(SearchPanelView searchPanelView) {
+        mDelegateHelper.setDelegateView(searchPanelView);
+    }
+
+    public void setBar(BaseStatusBar bar) {
+        mDelegateHelper.setBar(bar);
+    }
+
+    private int getGravity() {
+        if (isScreenPortrait()) {
+            return Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+        } else {
+            return Gravity.CENTER_VERTICAL | Gravity.RIGHT;
+        }
+    }
+
+    public WindowManager.LayoutParams getGesturePanelLayoutParams() {
+        WindowManager.LayoutParams lp;
+        if (isScreenPortrait()) {
+            lp  = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                    | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                    PixelFormat.TRANSLUCENT);
+        } else {
+            lp  = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.WRAP_CONTENT,WindowManager.LayoutParams.MATCH_PARENT,
+                    WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                    | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                    PixelFormat.TRANSLUCENT);
+        }
+        lp.gravity = getGravity();
+        lp.setTitle("SwipePanelSwipeView");
+        return lp;
+    }
+
+    public void updateLayout() {
+        LinearLayout.LayoutParams dragParams;
+        float dragHeight = (mGestureHeight * (mButtonHeight * 0.01f));
+        removeAllViews();
+        mDragButton.setBackgroundColor(Color.BLACK);
+        if (isScreenPortrait()) {
+            mDelegateHelper.setSwapXY(false);
+            dragParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (int) dragHeight);
+        } else {
+            mDelegateHelper.setSwapXY(true);
+            dragParams = new LinearLayout.LayoutParams((int) dragHeight, LinearLayout.LayoutParams.MATCH_PARENT);
+        }
+        mDragButton.setVisibility(View.INVISIBLE);
+        addView(mDragButton,dragParams);
+        invalidate();
+    }
+
+    public boolean isScreenPortrait() {
+        return res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -143,6 +143,7 @@ import com.android.internal.util.cm.WeatherControllerImpl;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.BatteryMeterView;
+import com.android.systemui.bliss.SearchPanelSwipeView;
 import com.android.systemui.BatteryLevelTextView;
 import com.android.systemui.DemoMode;
 import com.android.systemui.EventLogConstants;
@@ -704,6 +705,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private ScrimController mScrimController;
     private DozeScrimController mDozeScrimController;
 
+    private SearchPanelSwipeView mSearchPanelSwipeView;
+
     private final Runnable mAutohide = new Runnable() {
         @Override
         public void run() {
@@ -1036,6 +1039,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         } else {
             addAppCircleSidebar();
             addGestureAnywhereView();
+        }
+
+        try {
+            boolean showNav = mWindowManagerService.hasNavigationBar();
+            if (!showNav) {
+                mSearchPanelSwipeView = new SearchPanelSwipeView(mContext, this);
+                mWindowManager.addView(mSearchPanelSwipeView, mSearchPanelSwipeView.getGesturePanelLayoutParams());
+                updateSearchPanel();
+            }
+        } catch (RemoteException ex) {
+            // no window manager? good luck with that
         }
 
         // figure out which pixel-format to use for the status bar.
@@ -1512,6 +1526,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mNavigationBarView != null) {
             mNavigationBarView.setDelegateView(mSearchPanelView);
         }
+        if (mSearchPanelSwipeView != null) {
+            mSearchPanelSwipeView.setDelegateView(mSearchPanelView);
+        }
     }
 
     @Override
@@ -1644,6 +1661,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         prepareNavigationBarView(false);
 
         mWindowManager.updateViewLayout(mNavigationBarView, getNavigationBarLayoutParams());
+    }
+
+    private void repositionSearchPanelSwipeView() {
+        if (mSearchPanelSwipeView == null || !mSearchPanelSwipeView.isAttachedToWindow()) return;
+        mSearchPanelSwipeView.updateLayout();
+        mWindowManager.updateViewLayout(mSearchPanelSwipeView, mSearchPanelSwipeView.getGesturePanelLayoutParams());
+        updateSearchPanel();
     }
 
     private void notifyNavigationBarScreenOn(boolean screenOn) {
@@ -3932,6 +3956,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
             else if (Intent.ACTION_SCREEN_ON.equals(action)) {
                 mScreenOn = true;
+                repositionSearchPanelSwipeView();
                 notifyNavigationBarScreenOn(true);
             }
             else if (ACTION_DEMO.equals(action)) {