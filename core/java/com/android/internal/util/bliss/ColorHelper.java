bfb265cec2a9bd3ddde06f97f7065d81a17b0d29
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-04-25 01:02:44 +0200

    Status bar: Add battery color transition animations, (part 1):
    - Add a color transition animation to the battery text on color change
      (only battery style: "text", or if the text is set to "next to the icon", for now)
    
    Change-Id: Iadf25a8bbb47a904c684ef7b9bd7d372f25180de

---
+++ core/java/com/android/internal/util/bliss/ColorHelper.java
@@ -0,0 +1,33 @@
+/*
+* Copyright (C) 2015 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.bliss;
+
+import android.graphics.Color;
+
+public class ColorHelper {
+
+    public static int getBlendColor(int from, int to, float ratio) {
+        final float inverseRatio = 1f - ratio;
+
+        final float a = Color.alpha(to) * ratio + Color.alpha(from) * inverseRatio;
+        final float r = Color.red(to) * ratio + Color.red(from) * inverseRatio;
+        final float g = Color.green(to) * ratio + Color.green(from) * inverseRatio;
+        final float b = Color.blue(to) * ratio + Color.blue(from) * inverseRatio;
+
+        return Color.argb((int) a, (int) r, (int) g, (int) b);
+    }
+}
