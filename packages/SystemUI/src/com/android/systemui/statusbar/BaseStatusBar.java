71ce0616af7abbdfb045ff485e61bd08cf9a3dbd
Phillip Berndt <phillip.berndt@googlemail.com>
2015-05-08 10:19:42 +0200

    BaseStatusBar: Inform StatusBarService if a notification is flagged spam
    Without this change, if a notification that flashes the status LED is
    dismissed as spam by the user, the status LED continues to blink while
    the device is on standby (because the notification is not removed from
    NotificationManagerService.mLights) until the next boot.
    
    Change-Id: I30074adefa8f25cb21ae6550d523a91ebb943527

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1043,6 +1043,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                     values.put(PackageTable.PACKAGE_NAME, pkg);
                     mContext.getContentResolver().insert(SPAM_MESSAGE_URI, values);
                     removeNotification(sbn.getKey(), null);
+                    onNotificationClear(sbn);
                 }
             });
 
