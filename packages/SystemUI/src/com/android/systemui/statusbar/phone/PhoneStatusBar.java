2507c067898be31d82005f43cff6d2004c209bab
Daniel Koman <dankoman30@gmail.com>
2015-05-17 03:12:08 -0600

    Add Feature: Heads Up custom text/background color/transparency
    Ported by Team Bliss && @rogerb11
    
    Change-Id: I40e5b773265e2a358aa47729c4a3a9fe34b53817

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -278,6 +278,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     /** Allow some time inbetween the long press for back and recents. */
     private static final int LOCK_TO_APP_GESTURE_TOLERENCE = 100;
 
+    private final int HEADSUP_DEFAULT_BACKGROUNDCOLOR = 0x00ffffff;
+
     PhoneStatusBarPolicy mIconPolicy;
 
     // These are no longer handled by the policy, because we need custom strategies for them
@@ -394,6 +396,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private boolean mBlissLogo;
     private int mBlissLogoColor;
     private ImageView blissLogo;
+
+    // Heads Up Custom Colors
+    private int mHeadsUpCustomBg;
+    private int mHeadsUpCustomText;
 
     // battery
     private BatteryMeterView mBatteryView;
@@ -539,6 +545,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.PIE_CONTROLS),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_BG_COLOR),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_TEXT_COLOR),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.APP_SIDEBAR_POSITION),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -594,6 +606,18 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_BATTERY_STATUS_TEXT_COLOR))) {
                 updateBatteryLevelTextColor();
+             } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_BG_COLOR))) {
+                    mHeadsUpCustomBg = Settings.System.getIntForUser(
+                        mContext.getContentResolver(),
+                        Settings.System.HEADS_UP_BG_COLOR, HEADSUP_DEFAULT_BACKGROUNDCOLOR,
+                        UserHandle.USER_CURRENT);
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_TEXT_COLOR))) {
+                    mHeadsUpCustomText = Settings.System.getIntForUser(
+                        mContext.getContentResolver(),
+                        Settings.System.HEADS_UP_TEXT_COLOR, 0x00000000,
+                        UserHandle.USER_CURRENT);
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.QS_COLOR_SWITCH))) {
                     mQSCSwitch = Settings.System.getIntForUser(
@@ -1032,12 +1056,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mNotificationPanel.setBackground(new FastColorDrawable(context.getResources().getColor(
                     R.color.notification_panel_solid_background)));
         }
+
         if (ENABLE_HEADS_UP) {
             mHeadsUpNotificationView =
                     (HeadsUpNotificationView) View.inflate(context, R.layout.heads_up, null);
-            mHeadsUpNotificationView.setVisibility(View.GONE);
-            mHeadsUpNotificationView.setBar(this);
-
             mHeadsUpNotificationView.setVisibility(View.GONE);
             mHeadsUpNotificationView.setBar(this);
             mHeadsUpNotificationDecay = Settings.System.getIntForUser(
@@ -1046,6 +1068,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     res.getInteger(R.integer.heads_up_notification_decay),
                     UserHandle.USER_CURRENT);
         }
+
         if (MULTIUSER_DEBUG) {
             mNotificationPanelDebugText = (TextView) mNotificationPanel.findViewById(
                     R.id.header_debug_info);
@@ -1086,9 +1109,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 return false;
             }
         });
-        
-        if (mRecreating) {
-        } else {
+
+        if (!mRecreating) {
             addAppCircleSidebar();
             addGestureAnywhereView();
         }
@@ -1855,9 +1877,22 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             if (DEBUG) Log.d(TAG, "launching notification in heads up mode");
             Entry interruptionCandidate = new Entry(notification, null);
             ViewGroup holder = mHeadsUpNotificationView.getHolder();
-            if (inflateViewsForHeadsUp(interruptionCandidate, holder)) {
+
+            // get text color value
+            int mHeadsUpCustomTextColor = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.HEADS_UP_TEXT_COLOR,
+                0x00000000, UserHandle.USER_CURRENT);
+
+            if (inflateViews(interruptionCandidate, holder, true, mHeadsUpCustomTextColor)) {
+
+                // get background value
+                int mHeadsUpCustomBg = Settings.System.getIntForUser(
+                    mContext.getContentResolver(), Settings.System.HEADS_UP_BG_COLOR,
+                    HEADSUP_DEFAULT_BACKGROUNDCOLOR, UserHandle.USER_CURRENT);
+
                 // 1. Populate mHeadsUpNotificationView
-                mHeadsUpNotificationView.showNotification(interruptionCandidate);
+                mHeadsUpNotificationView.showNotification(interruptionCandidate,
+                    mHeadsUpCustomBg);
 
                 // do not show the notification in the shade, yet.
                 return;
