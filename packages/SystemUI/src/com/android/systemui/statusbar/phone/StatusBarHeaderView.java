1543429e3b4e8e542d3799250b7649949a00db87
dexmc <dextermcdew@gmail.com>
2015-06-04 08:57:19 -0700

    SystemUI: Add longpress action for TaskManager button
    - long pressing on TaskManager button opens Running Apps screen.
    
    Inspired by commits from @Altaf-Mahdi and @snak3ater.
    
    Change-Id: I6cf5576c74f923bc81ba421b1d6411ea3c2852de
    Signed-off-by: dexmc <dextermcdew@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -219,6 +219,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         mSettingsButton = findViewById(R.id.settings_button);
         mSettingsButton.setOnClickListener(this);
         mTaskManagerButton = findViewById(R.id.task_manager_button);
+        mTaskManagerButton.setOnLongClickListener(this);
         mHeadsUpButton = findViewById(R.id.heads_up_button);
         mHeadsUpButton.setOnClickListener(this);
         mHeadsUpButton.setOnLongClickListener(mLongClickListener);
@@ -732,6 +733,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             triggerPowerMenuDialog();
         } else if (mStatusBarPowerMenuStyle == STATUS_BAR_POWER_MENU_INVERTED) {
             goToSleep();
+        } else if (v == mTaskManagerButton) {
+            startTaskManagerLongClickActivity();
         }
         return false;
     }
@@ -847,6 +850,13 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.setClassName("com.android.settings",
             "com.android.settings.Settings$HeadsUpSettingsActivity");
+        mActivityStarter.startActivity(intent, true /* dismissShade */);
+    }
+
+    private void startTaskManagerLongClickActivity() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setClassName("com.android.settings",
+            "com.android.settings.Settings$RunningServicesActivity");
         mActivityStarter.startActivity(intent, true /* dismissShade */);
     }
 
