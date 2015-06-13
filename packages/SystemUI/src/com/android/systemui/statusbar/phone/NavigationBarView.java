9d14acd18c39a66c6a02cc106a4b292f3fc2d130
tobitege <tobiasteschner@googlemail.com>
2015-06-13 18:51:41 +0200

    Revert "Navbar: tint and buttons loading fixup"
    Reverted due to navbar tint color gotten broken. :(
    
    This reverts commit 715aec2a8414eab1af2ffca4e85419dd3c80ddd4.
    
    Change-Id: I591973042daddbb6d99acea66489d8a69c34c9be

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -877,10 +877,7 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
                     getButtonsArray(mButtonContainerStrings[j]));
             }
         }
-        if (lastgood == -1) {
-            mCurrentLayout = 0;
-            mAllButtonContainers.put(0, mDefaultLayoutArray);
-        } else if (mCurrentLayout > lastgood) {
+        if (mCurrentLayout > lastgood) {
             mCurrentLayout = lastgood;
         }
         setupNavigationButtons(mAllButtonContainers.get(mCurrentLayout));
@@ -965,6 +962,9 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
                     addSeparator(navButtons, landscape, 0, stockThreeButtonLayout ? 1f : 0.5f);
                     addSeparator(lightsOut, landscape, 0, stockThreeButtonLayout ? 1f : 0.5f);
                 }
+            } else if (!mLegacyMenu) { // to fix slim recents
+                addSeparator(navButtons, landscape, mTablet ? (int) mMenuButtonWidth : separatorSize, 0f);
+                addSeparator(lightsOut, landscape, mTablet ? (int) mMenuButtonWidth : separatorSize, 0f);
             }
 
             // add the custom buttons
