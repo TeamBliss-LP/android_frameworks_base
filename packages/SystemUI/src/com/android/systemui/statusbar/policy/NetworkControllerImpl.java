305214de3a898be2db5fabf8284ac840d0a56217
Danny Baumann <dannybaumann@web.de>
2015-06-23 08:34:12 +0200

    Recalculate emergency after adding or removing mobile controllers.
    We might have shown 'emergency calls only' due to the lack of
    controllers before [isEmergencyOnly() returns true if the voice sub
    controller is missing], so make sure to properly hide it when adding
    controllers.
    
    Change-Id: Iad3762962f5d88df20599818b798cc0018e24e90

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -516,6 +516,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         // inet condition and airplane mode.
         pushConnectivityToSignals();
         updateAirplaneMode(true /* force */);
+        recalculateEmergency();
     }
 
     @VisibleForTesting
