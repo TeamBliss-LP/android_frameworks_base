c1dd4cd91c8d2b4ef31897ee8cedc98d40fe0c92
APOPHIS9283 <steaven.android@gmail.com>
2015-06-27 19:19:22 -0400

    Revert "[WIP]Base: Forward Port: Floating window with multitasking"
    This reverts commit 216a08195281389c369183e493d0fed121915229.
    
    Change-Id: Ica73c15df5ea2ab317d3d04505e92f9125938d68

--- core/java/android/view/Window.java
+++ core/java/android/view/Window.java
@@ -202,8 +202,6 @@ public abstract class Window {
 
     private boolean mDestroyed;
 
-    public boolean mIsFloatingWindow = false;
-
     // The current window attributes.
     private final WindowManager.LayoutParams mWindowAttributes =
         new WindowManager.LayoutParams();
