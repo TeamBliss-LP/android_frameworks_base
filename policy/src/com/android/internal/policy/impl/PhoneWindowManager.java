b58ac98d7f97d71f3abe8b0a86e5dfb42856e462
tobitege <tobiasteschner@googlemail.com>
2015-06-27 20:34:40 -0700

    Revert "Revert "[WIP]Base: Forward Port: Floating window with multitasking""
    This reverts commit c1dd4cd91c8d2b4ef31897ee8cedc98d40fe0c92.
    
    Change-Id: Ibe2cae006488475e9ee7850f373564536fd3a339

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4563,6 +4563,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
+    public Rect getContentRect() {
+        return new Rect(mContentLeft, mContentTop, mContentRight, mContentBottom);
+    }
+
     /** {@inheritDoc} */
     @Override
     public void layoutWindowLw(WindowState win, WindowState attached) {
