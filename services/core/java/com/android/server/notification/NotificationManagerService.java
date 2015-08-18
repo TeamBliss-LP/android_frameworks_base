5cb72715a0efab8d93b312ff4fe7f76cbb8c6445
AdrianDC <radian.dc@gmail.com>
2015-08-18 19:33:18 +0200

    LEDs Brightness [2/2]: Lights notifications brightness support
    Implement the support of an overall brightness control for the LEDs.
    
    The setting is deactivated by default
    and will be ignored by the unimplemented phones.
    Current LibLights will simply not use the new variable.
    
    Changes includes :
      frameworks/base
      packages/Apps/Settings
    
    Change-Id: I1c0de01b1c6a2a2cf1432028a2e69f90b2373b2c
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -201,6 +201,9 @@ public class NotificationManagerService extends SystemService {
     /** notification_enqueue status value for an ignored notification. */
     private static final int EVENTLOG_ENQUEUE_STATUS_IGNORED = 2;
 
+    /** notification light maximum brightness value to use. */
+    private static final int LIGHT_BRIGHTNESS_MAXIMUM = 255;
+
     private IActivityManager mAm;
     AudioManager mAudioManager;
     StatusBarManagerInternal mStatusBar;
@@ -218,6 +221,9 @@ public class NotificationManagerService extends SystemService {
 
     private int mDefaultNotificationLedOff;
     private long[] mDefaultVibrationPattern;
+
+    private boolean mAdjustableNotificationLedBrightness;
+    private int mNotificationLedBrightnessLevel = LIGHT_BRIGHTNESS_MAXIMUM;
 
     private boolean mScreenOnEnabled = false;
     private boolean mScreenOnDefault = false;
@@ -895,6 +901,12 @@ public class NotificationManagerService extends SystemService {
                     this, UserHandle.USER_ALL);
             resolver.registerContentObserver(MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD_URI,
                     false, this, UserHandle.USER_ALL);
+
+            if (mAdjustableNotificationLedBrightness) {
+                resolver.registerContentObserver(Settings.System.getUriFor(
+                        Settings.System.NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL),
+                        false, this, UserHandle.USER_ALL);
+            }
             update(null);
         }
 
@@ -932,6 +944,13 @@ public class NotificationManagerService extends SystemService {
                 parseNotificationPulseCustomValuesString(Settings.System.getStringForUser(resolver,
                         Settings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES,
                         UserHandle.USER_CURRENT));
+            }
+
+            // Notification LED brightness
+            if (mAdjustableNotificationLedBrightness) {
+                mNotificationLedBrightnessLevel = Settings.System.getIntForUser(resolver,
+                        Settings.System.NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL,
+                        LIGHT_BRIGHTNESS_MAXIMUM, UserHandle.USER_CURRENT);
             }
 
             // Notification lights with screen on
@@ -1070,6 +1089,9 @@ public class NotificationManagerService extends SystemService {
                 R.array.config_notificationFallbackVibePattern,
                 VIBRATE_PATTERN_MAXLEN,
                 DEFAULT_VIBRATE_PATTERN);
+
+        mAdjustableNotificationLedBrightness = resources.getBoolean(
+                com.android.internal.R.bool.config_adjustableNotificationLedBrightness);
 
         mUseAttentionLight = resources.getBoolean(R.bool.config_useAttentionLight);
 
@@ -3084,6 +3106,9 @@ public class NotificationManagerService extends SystemService {
                 ledOnMS = ledno.ledOnMS;
                 ledOffMS = ledno.ledOffMS;
             }
+
+            // update the LEDs modes variables
+            mNotificationLight.setModes(mNotificationLedBrightnessLevel);
 
             if (mNotificationPulseEnabled) {
                 // pulse repeatedly
