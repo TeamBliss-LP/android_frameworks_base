c1c037f7e593cb1e1185721efb4100c5b819e7dc
BlackDragon <blackdragon.fusionteam@gmail.com>
2015-09-09 18:07:01 -0700

    On-The-Go Mode (1/2)
    Activate from PowerMenu.
    
    Todo: Add qs tile, already included the qs tile drawables.
          Ability to use the flashlight.
          Add Shakelistener.
    
    Author original code NamelessRom
    
    Change-Id: If3db29575cefd4a46ae387a5e3f7464e62bd3edd
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4964,6 +4964,36 @@ public final class Settings {
         public static final String APP_SIDEBAR_SHOW_TRIGGER = "app_sidebar_show_trigger";
 
         /**
+         * If On-The-Go should be displayed at the power menu.
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_ONTHEGO_ENABLED = "power_menu_onthego_enabled";
+
+        /**
+         * The alpha value of the On-The-Go overlay.
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_ALPHA = "on_the_go_alpha";
+
+        /**
+         * Whether the service should restart itself or not.
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_SERVICE_RESTART = "on_the_go_service_restart";
+
+        /**
+         * The camera instance to use.
+         * 0 = Rear Camera
+         * 1 = Front Camera
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
+
+        /**
          * Disable ads (HFM)
          * @hide
          */
