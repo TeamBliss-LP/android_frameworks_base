43b59b1786f561b83380dd264785110d0b44e287
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-09-30 13:28:32 +0000

    SystemUI: add vibration for task manager and multi user button
    Change-Id: Ie1a4b55c8a3f1de5440cdf192373548b5b5a79bc

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1946,6 +1946,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             public void onClick(View arg0) {
                 mShowTaskList = !mShowTaskList;
                 mNotificationPanel.setTaskManagerVisibility(mShowTaskList);
+                mQSPanel.vibrateTile(20);
             }
         });
         if (mRecreating) {
